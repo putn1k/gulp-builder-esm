193dd94861f547c53b9b9cd716a882098cdbba53
putn1k <skachok11@yandex.ru>
2022-11-09 17:29:42 +0300

    добавлены утилитарные функции

---
+++ src/js/modules/utils.js
@@ -0,0 +1,50 @@
+const debounce = ( cb, delay ) => {
+  let timer;
+
+  return function( ...args ) {
+    clearTimeout( timer );
+    timer = setTimeout( () => {
+      cb.apply( this, args );
+    }, delay );
+  };
+};
+
+const throttle = ( cb, delay ) => {
+  let isWaiting = false;
+  let savedThis = null;
+  let savedArgs = null;
+
+  return function wrapper( ...args ) {
+    if ( isWaiting ) {
+      savedThis = this;
+      savedArgs = args;
+      return;
+    }
+
+    cb.apply( this, args );
+    isWaiting = true;
+    setTimeout( () => {
+      isWaiting = false;
+      if ( savedThis ) {
+        wrapper.apply( savedThis, savedArgs );
+        savedThis = null;
+        savedArgs = null;
+      }
+    }, delay );
+  };
+};
+
+const disableSubmitBtn = ( form ) => {
+  form.querySelector( '[type="submit"]' ).setAttribute( 'disabled', 'disabled' );
+};
+
+const enableSubmitBtn = ( form ) => {
+  form.querySelector( '[type="submit"]' ).removeAttribute( 'disabled' );
+};
+
+export {
+  debounce,
+  throttle,
+  disableSubmitBtn,
+  enableSubmitBtn,
+};
